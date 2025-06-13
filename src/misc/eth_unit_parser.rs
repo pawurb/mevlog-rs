df7888783f4311ca6d0a3c5c55987a7566b12024
pawurb <contact@pawelurbanek.com>
2025-06-13 22:01:31 +0200

    feat: Add ETH units

--- src/misc/eth_unit_parser.rs
+++ src/misc/eth_unit_parser.rs
@@ -6,8 +6,16 @@ use revm::primitives::U256;
 #[derive(Debug, Clone, Copy)]
 pub enum EthUnit {
     Wei,
-    Gwei,
+    Kwei,   // Babbage, Femtoether
+    Mwei,   // Lovelace, Picoether
+    Gwei,   // Shannon, Nanoether, Nano
+    Szabo,  // Microether, Micro
+    Finney, // Milliether, Milli
     Ether,
+    Kether, // Grand
+    Mether,
+    Gether,
+    Tether,
 }
 
 impl FromStr for EthUnit {
@@ -16,8 +24,16 @@ impl FromStr for EthUnit {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s.to_lowercase().as_str() {
             "wei" => Ok(EthUnit::Wei),
-            "gwei" => Ok(EthUnit::Gwei),
+            "kwei" | "babbage" | "femtoether" => Ok(EthUnit::Kwei),
+            "mwei" | "lovelace" | "picoether" => Ok(EthUnit::Mwei),
+            "gwei" | "shannon" | "nanoether" | "nano" => Ok(EthUnit::Gwei),
+            "szabo" | "microether" | "micro" => Ok(EthUnit::Szabo),
+            "finney" | "milliether" | "milli" => Ok(EthUnit::Finney),
             "ether" | "eth" => Ok(EthUnit::Ether),
+            "kether" | "grand" => Ok(EthUnit::Kether),
+            "mether" => Ok(EthUnit::Mether),
+            "gether" => Ok(EthUnit::Gether),
+            "tether" => Ok(EthUnit::Tether),
             _ => bail!("Unknown unit: {}", s),
         }
     }
@@ -27,8 +43,16 @@ impl EthUnit {
     pub fn multiplier(&self) -> U256 {
         match self {
             EthUnit::Wei => U256::from(1),
+            EthUnit::Kwei => U256::from(10).pow(U256::from(3)),
+            EthUnit::Mwei => U256::from(10).pow(U256::from(6)),
             EthUnit::Gwei => U256::from(10).pow(U256::from(9)),
+            EthUnit::Szabo => U256::from(10).pow(U256::from(12)),
+            EthUnit::Finney => U256::from(10).pow(U256::from(15)),
             EthUnit::Ether => U256::from(10).pow(U256::from(18)),
+            EthUnit::Kether => U256::from(10).pow(U256::from(21)),
+            EthUnit::Mether => U256::from(10).pow(U256::from(24)),
+            EthUnit::Gether => U256::from(10).pow(U256::from(27)),
+            EthUnit::Tether => U256::from(10).pow(U256::from(30)),
         }
     }
 }
@@ -131,14 +155,37 @@ mod tests {
             "Should parse raw integer as wei"
         );
 
-        // Test gwei values
+        // Test all units with their factors
+        assert_eq!(
+            parse_eth_value("1kwei").unwrap(),
+            U256::from(10).pow(U256::from(3)),
+            "Should convert 1 kwei to wei correctly"
+        );
+
+        assert_eq!(
+            parse_eth_value("1mwei").unwrap(),
+            U256::from(10).pow(U256::from(6)),
+            "Should convert 1 mwei to wei correctly"
+        );
+
         assert_eq!(
             parse_eth_value("5gwei").unwrap(),
             U256::from(5) * U256::from(10).pow(U256::from(9)),
             "Should convert 5 gwei to wei correctly"
         );
 
-        // Test ether values
+        assert_eq!(
+            parse_eth_value("1szabo").unwrap(),
+            U256::from(10).pow(U256::from(12)),
+            "Should convert 1 szabo to wei correctly"
+        );
+
+        assert_eq!(
+            parse_eth_value("1finney").unwrap(),
+            U256::from(10).pow(U256::from(15)),
+            "Should convert 1 finney to wei correctly"
+        );
+
         assert_eq!(
             parse_eth_value("1ether").unwrap(),
             U256::from(10).pow(U256::from(18)),
@@ -149,6 +196,93 @@ mod tests {
             parse_eth_value("0.5ether").unwrap(),
             U256::from(10).pow(U256::from(18)) / U256::from(2),
             "Should convert 0.5 ether to wei correctly"
+        );
+
+        assert_eq!(
+            parse_eth_value("1kether").unwrap(),
+            U256::from(10).pow(U256::from(21)),
+            "Should convert 1 kether to wei correctly"
+        );
+
+        assert_eq!(
+            parse_eth_value("1mether").unwrap(),
+            U256::from(10).pow(U256::from(24)),
+            "Should convert 1 mether to wei correctly"
+        );
+
+        assert_eq!(
+            parse_eth_value("1gether").unwrap(),
+            U256::from(10).pow(U256::from(27)),
+            "Should convert 1 gether to wei correctly"
+        );
+
+        assert_eq!(
+            parse_eth_value("1tether").unwrap(),
+            U256::from(10).pow(U256::from(30)),
+            "Should convert 1 tether to wei correctly"
+        );
+    }
+
+    #[test]
+    fn test_unit_aliases() {
+        // Test aliases for kwei
+        assert_eq!(
+            parse_eth_value("1babbage").unwrap(),
+            parse_eth_value("1kwei").unwrap()
+        );
+        assert_eq!(
+            parse_eth_value("1femtoether").unwrap(),
+            parse_eth_value("1kwei").unwrap()
+        );
+
+        // Test aliases for mwei
+        assert_eq!(
+            parse_eth_value("1lovelace").unwrap(),
+            parse_eth_value("1mwei").unwrap()
+        );
+        assert_eq!(
+            parse_eth_value("1picoether").unwrap(),
+            parse_eth_value("1mwei").unwrap()
+        );
+
+        // Test aliases for gwei
+        assert_eq!(
+            parse_eth_value("1shannon").unwrap(),
+            parse_eth_value("1gwei").unwrap()
+        );
+        assert_eq!(
+            parse_eth_value("1nanoether").unwrap(),
+            parse_eth_value("1gwei").unwrap()
+        );
+        assert_eq!(
+            parse_eth_value("1nano").unwrap(),
+            parse_eth_value("1gwei").unwrap()
+        );
+
+        // Test aliases for szabo
+        assert_eq!(
+            parse_eth_value("1microether").unwrap(),
+            parse_eth_value("1szabo").unwrap()
+        );
+        assert_eq!(
+            parse_eth_value("1micro").unwrap(),
+            parse_eth_value("1szabo").unwrap()
+        );
+
+        // Test aliases for finney
+        assert_eq!(
+            parse_eth_value("1milliether").unwrap(),
+            parse_eth_value("1finney").unwrap()
+        );
+        assert_eq!(
+            parse_eth_value("1milli").unwrap(),
+            parse_eth_value("1finney").unwrap()
+        );
+
+        // Test aliases for kether
+        assert_eq!(
+            parse_eth_value("1grand").unwrap(),
+            parse_eth_value("1kether").unwrap()
         );
     }
 }
