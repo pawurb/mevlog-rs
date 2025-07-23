e53a8bfd38a05419e0c13e3b786eae753b22da46
pawurb <contact@pawelurbanek.com>
2025-07-24 00:31:14 +0200

    chore: Add revm tracing integration tests

--- tests/cli_tests.rs
+++ tests/cli_tests.rs
@@ -5,7 +5,7 @@ pub mod tests {
     use eyre::Result;
 
     #[test]
-    fn test_cli_search() -> Result<()> {
+    fn test_cli_search_rpc() -> Result<()> {
         let cmd = Command::new("cargo")
             .arg("run")
             .arg("--bin")
@@ -22,12 +22,30 @@ pub mod tests {
             .output()
             .expect("failed to execute CLI");
 
-        if !cmd.stderr.is_empty() {
-            println!(
-                "stderr: {:?}",
-                String::from_utf8(cmd.stderr.clone()).unwrap()
-            );
-        }
+        let output = String::from_utf8(cmd.stdout).unwrap();
+        println!("output: {output}");
+        assert!(output.contains("Real Gas Price:    18253.30 GWEI"));
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_cli_search_revm() -> Result<()> {
+        let cmd = Command::new("cargo")
+            .arg("run")
+            .arg("--bin")
+            .arg("mevlog")
+            .arg("search")
+            .arg("-b")
+            .arg("22045570")
+            .arg("-p")
+            .arg("0")
+            .arg("--rpc-url")
+            .arg(std::env::var("ETH_RPC_URL").expect("ETH_RPC_URL must be set"))
+            .arg("--trace")
+            .arg("revm")
+            .output()
+            .expect("failed to execute CLI");
 
         let output = String::from_utf8(cmd.stdout).unwrap();
         println!("output: {output}");
@@ -37,7 +55,7 @@ pub mod tests {
     }
 
     #[test]
-    fn test_cli_tx() -> Result<()> {
+    fn test_cli_tx_rpc() -> Result<()> {
         let cmd = Command::new("cargo")
             .arg("run")
             .arg("--bin")
@@ -48,6 +66,28 @@ pub mod tests {
             .arg(std::env::var("ETH_RPC_URL").expect("ETH_RPC_URL must be set"))
             .arg("--trace")
             .arg("rpc")
+            .output()
+            .expect("failed to execute CLI");
+
+        let output = String::from_utf8(cmd.stdout).unwrap();
+        println!("output: {output}");
+        assert!(output.contains("Real Gas Price:    18253.30 GWEI"));
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_cli_tx_revm() -> Result<()> {
+        let cmd = Command::new("cargo")
+            .arg("run")
+            .arg("--bin")
+            .arg("mevlog")
+            .arg("tx")
+            .arg("0x06fed3f7dc71194fe3c2fd379ef1e8aaa850354454ea9dd526364a4e24853660")
+            .arg("--rpc-url")
+            .arg(std::env::var("ETH_RPC_URL").expect("ETH_RPC_URL must be set"))
+            .arg("--trace")
+            .arg("revm")
             .output()
             .expect("failed to execute CLI");
 
